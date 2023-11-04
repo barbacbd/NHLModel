230ece1379bd03593e3dea868ddc301e5abfc0ca
Brent Barbachem <barbacbd@gmail.com>
2023-11-04 12:20:17 -0400

    Updated to ensure that a minimum number of games have been player. Also added some debugging/info statements.

--- discrete/poisson/poisson.py
+++ discrete/poisson/poisson.py
@@ -7,6 +7,15 @@ from scipy.stats import poisson
 import matplotlib.pyplot as plt
 from pandas import DataFrame
 from statistics import mean
+from termcolor import colored
+
+
+MIN_GAMES_FOR_VALID_RECORD = 5
+
+
+def _alertNotEnoughGames(teamName, season):
+    print(colored(f'{teamName} does not have a valid record for {season}.', 'blue'))
+    print(colored(f'Please ensure more than {MIN_GAMES_FOR_VALID_RECORD} home and away games exist. ', 'blue'))        
 
 
 def readStatisticsFile():
@@ -22,47 +31,6 @@ def readStatisticsFile():
         jsonData = loads(jsonFile.read())
 
     return jsonData
-
-
-def parseArguments():
-    """Parse the arguements for the program by reading in the static file that
-    contains the basic statistics for all teams and all seasons.
-    """
-    # get the year then implement argparse 
-    currentYear = datetime.now().year
-    questions = [
-        inquirer.Text('year', message="Enter the year for the season start.", default=currentYear-1),
-    ]
-    answers = inquirer.prompt(questions)
-    
-    year = int(answers["year"])
-    if NHL_FIRST_SEASON > year > currentYear:
-        # TODO: Add debugging logger here
-        year = currentYear - 1
-
-    teamNames = []
-    season = f"{year}{year+1}"
-
-    jsonData = readStatisticsFile()
-    if season in jsonData:
-        for k, v in jsonData[season].items():
-            teamNames.append(v["name"])
-
-    if not teamNames:
-        return None
-    teamNames.sort()
-
-    questions = [
-        inquirer.List("homeTeam", message="Name of the home team for analysis.", choices=teamNames),
-        inquirer.List("awayTeam", message="Name of the away team for analysis.", choices=teamNames)
-    ]
-    answers = inquirer.prompt(questions)
-
-    return {
-        "season": season,
-        "homeTeam": answers["homeTeam"],
-        "awayTeam": answers["awayTeam"]
-    }
 
 
 def getJsonRecord(season, teamName=None):
@@ -94,8 +62,85 @@ def getTotalGoals(season):
     return goalsScored
 
 
-def calculate(season, homeTeam, awayTeam):
+def parseArguments():
+    """Parse the arguements for the program by reading in the static file that
+    contains the basic statistics for all teams and all seasons.
+    """
+    def _askAndVerifyTeam(questions, season, teamType):
+        answers = inquirer.prompt(questions)
+        teamName = answers[teamType]
+        teamRecord = getJsonRecord(season, teamName)
+        if not teamRecord or \
+            teamRecord["homeGames"] < MIN_GAMES_FOR_VALID_RECORD or \
+            teamRecord["awayGames"] < MIN_GAMES_FOR_VALID_RECORD:
+            # ensure that there is a valid record and that there are enough games
+            _alertNotEnoughGames(teamName, season)
+            return None
+        return teamName
 
+    # Ask for the year/season for analysis
+    currentYear = datetime.now().year
+    questions = [
+        inquirer.Text('year', message="Enter the year for the season start.", default=currentYear-1),
+    ]
+    answers = inquirer.prompt(questions)
+    year = int(answers["year"])
+
+    while True:
+        # get the year then implement argparse 
+        if NHL_FIRST_SEASON > year > currentYear:
+            # TODO: Add debugging logger here
+            year = currentYear - 1
+
+        teamNames = []
+        season = f"{year}{year+1}"
+        print(colored(f'selected {season} season.', 'blue'))
+
+        jsonData = readStatisticsFile()
+        if season in jsonData:
+            for k, v in jsonData[season].items():
+                teamNames.append(v["name"])
+
+        if not teamNames:
+            # Failed to find the any teams ... try with the year before
+            print(colored(f'failed to find teams for {season} season, retrying with previous season.', 'blue'))
+            year -= 1
+            continue 
+
+        # sort alphabetically for easier search
+        teamNames.sort()
+
+        homeTeamName = _askAndVerifyTeam(
+            [inquirer.List("homeTeam", message="Name of the home team for analysis.", choices=teamNames)],
+            season,
+            "homeTeam"
+        )
+        if not homeTeamName:
+            year -= 1
+            continue
+
+        # remove the home team so that the user does not select the same team.
+        teamNames.remove(homeTeamName)
+
+
+        awayTeamName = _askAndVerifyTeam(
+            [inquirer.List("awayTeam", message="Name of the away team for analysis.", choices=teamNames)],
+            season,
+            "awayTeam"
+        )
+        if not awayTeamName:
+            year -= 1
+            continue
+
+        # return to exit the loop
+        return {
+            "season": season,
+            "homeTeam": homeTeamName,
+            "awayTeam": awayTeamName
+        }
+
+
+def calculate(season, homeTeam, awayTeam):
     goalsScored = getTotalGoals(season)
     goalScoredMean, maxGoalsScored = round(mean(goalsScored), 2), max(goalsScored)
     homeTeamRecord = getJsonRecord(season, homeTeam)
@@ -183,7 +228,7 @@ def main():
     print(f"{args['homeTeam']} (HOME) win percentage: {round(homeWinPercent*100.0, 2)}")
     print(f"{args['awayTeam']} (AWAY) win percentage: {round(awayWinPercent*100.0, 2)}")
     print(f"Regulation tie percent: {round(regulationTiePercent*100.0,2)}")
-    print(f"Expected Score (HOME) {int(round(homeGoals, 0))} - {int(round(awayGoals, 0))} (AWAY)")
+    print(f"Expected Regulation Score (HOME) {int(round(homeGoals, 0))} - {int(round(awayGoals, 0))} (AWAY)")
 
 
 
