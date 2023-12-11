5ca933ca732a65eba9c410010e3e1e6295dd7f7f
Brent Barbachem <barbacbd@gmail.com>
2023-12-11 12:16:42 -0500

    Added the way to generate the same dataset (minus a couple fields that aren't considered useful).

--- src/nhl_score_prediction/support/GenerateANNDataset.py
+++ src/nhl_score_prediction/support/GenerateANNDataset.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from json import loads
 import os
 from os import mkdir, remove
-from os.path import exists, join as path_join
+from os.path import exists, join as path_join, dirname, abspath
 from shutil import rmtree
 from nhl_score_prediction.poisson.poisson import parseSeasonEvents
 import pandas as pd
@@ -27,6 +27,32 @@ _staticBoxScoreTeamData = {
     "hits": ["teamStats", "teamSkaterStats", "hits"],
 }
 
+# TODO: Need to read in the new data. And grab the data below if it exists. Create
+# the dataframe and/or spreadsheet for the data. 
+_staticBoxScoreTeamDataNew = {
+    "teamId": ["id"],
+    "teamName": ["name", "default"],
+    "triCode": ["abbrev"],
+    "goals": ["score"],
+    "pim": ["pim"],
+    "shots": ["sog"],
+    "faceOffWinPercentage": ["faceoffWinningPctg"],
+    "blocked": ["blocks"],
+    "hits": ["hits"],
+}
+
+def _parsePPDataNew(powerPlayData):
+    """Parse the power play information from the "new" boxscore data that 
+    can be retrieved from the new API.
+    """
+    sp = powerPlayData.split("/")
+    success, opportunities = int(sp[0]), int(sp[1])
+    return {
+        "powerPlayPercentage": round(eval(powerPlayData) * 100.0, 2) if opportunities > 0 else 0.0,
+        "powerPlayGoals": success,
+        "powerPlayOpportunities": opportunities,
+    }
+
 
 def _parseInternalTeamData(boxScoreValue, dataList):
     subVar = boxScoreValue
@@ -46,6 +72,19 @@ def _parseInternalBoxScoreTeams(teamDict):
         ret[key] = _parseInternalTeamData(teamDict, value)
     
     return ret
+
+
+def _parseInternalBoxScoreTeamsNew(teamDict):
+    ret = {}
+
+    for key, value in _staticBoxScoreTeamDataNew.items():
+        ret[key] = _parseInternalTeamData(teamDict, value)
+    
+    ppData = _parsePPDataNew(teamDict["powerPlayConversion"])
+    ret.update(ppData)
+
+    return ret
+
 
 
 def _parseInternalBoxScorePlayers(teamDict):
@@ -126,6 +165,117 @@ def parseBoxScore(boxscore):
     return homeTeamData, awayTeamData
 
 
+def _parseInternalBoxScorePlayersNew(teamDict):
+    numPlayers = 0
+    numGoalies = 0
+
+    # Setting all to None so that we can figure out where stats are missing later
+
+    skaterDict = {
+        "assists": 0,
+    }
+    goalieDict = {
+        "saves": 0,
+        "powerPlaySaves": 0,
+        "shortHandedSaves": 0,
+        "evenSaves": 0,
+        "shortHandedShotsAgainst": 0,
+        "evenShotsAgainst": 0,
+        "powerPlayShotsAgainst": 0,
+        "savePercentage": 0,
+        "powerPlaySavePercentage": 0,
+        "shortHandedSavePercentage": 0,
+        "evenStrengthSavePercentage": 0
+    }
+
+    for playerType, playerValues in teamDict.items():
+
+        if playerType in ("forwards", "defense",):
+            for playerData in playerValues:
+                spTOI = playerData["toi"].split(":")
+                timeOnIce = int(spTOI[0]) * 60 + int(spTOI[1])
+                if timeOnIce > 0:
+                    numPlayers += 1
+                    skaterDict['assists'] += playerData["assists"]
+
+        elif playerType in ("goalies",):
+            for playerData in playerValues:
+                spTOI = playerData["toi"].split(":")
+                timeOnIce = int(spTOI[0]) * 60 + int(spTOI[1])
+                if timeOnIce > 0:
+                    numGoalies += 1
+                    sp = playerData["evenStrengthShotsAgainst"].split("/")
+                    if len(sp) == 2:
+                        goalieDict["evenShotsAgainst"] = goalieDict["evenShotsAgainst"] + int(sp[1])
+                        goalieDict["saves"] = goalieDict["saves"] + int(sp[0])
+                        goalieDict["evenSaves"] = goalieDict["evenSaves"] + int(sp[0])
+
+                    sp = playerData["powerPlayShotsAgainst"].split("/")
+                    if len(sp) == 2:
+                        goalieDict["powerPlayShotsAgainst"] += int(sp[1])
+                        goalieDict["saves"] += int(sp[0])
+                        goalieDict["powerPlaySaves"] += int(sp[0])
+
+                    sp = playerData["shorthandedShotsAgainst"].split("/")
+                    if len(sp) == 2:
+                        goalieDict["shortHandedShotsAgainst"] += int(sp[1])
+                        goalieDict["saves"] += int(sp[0])
+                        goalieDict["shortHandedSaves"] += int(sp[0])
+
+    totalShotsAgainst = goalieDict["evenShotsAgainst"] + \
+        goalieDict["powerPlayShotsAgainst"] + goalieDict["shortHandedShotsAgainst"]
+
+    goalieDict.update({
+        "savePercentage": 
+            round(float(goalieDict["saves"]) / float(totalShotsAgainst) * 100.0, 2) if totalShotsAgainst > 0 else 0.0,
+        "powerPlaySavePercentage": 
+            round(float(goalieDict["powerPlaySaves"]) / float(goalieDict["powerPlayShotsAgainst"]) * 100.0, 2) if \
+                goalieDict["powerPlayShotsAgainst"] > 0 else 0.0,
+        "shortHandedSavePercentage": 
+            round(float(goalieDict["shortHandedSaves"]) / float(goalieDict["shortHandedShotsAgainst"]) * 100.0, 2) if \
+                goalieDict["shortHandedShotsAgainst"] > 0 else 0.0,
+        "evenStrengthSavePercentage": 
+            round(float(goalieDict["evenSaves"]) / float(goalieDict["evenShotsAgainst"]) * 100.0, 2) if \
+                goalieDict["evenShotsAgainst"] > 0 else 0.0,
+    })
+
+    skaterDict.update(goalieDict)
+    skaterDict["numGoalies"] = numGoalies
+    skaterDict["numPlayers"] = numPlayers
+
+    return skaterDict
+
+
+def parseBoxScoreNew(boxscore):
+    """Parse the box score (new version). This version of the box score should be read
+    from the NHLOpenSeason.json file. The box score will serve as a great starting point
+    for the neural net dataset. The boxscore differs from year to year, but the
+    main data points will be present.
+    """
+    homeTeamData = _parseInternalBoxScoreTeamsNew(boxscore["homeTeam"])
+    awayTeamData = _parseInternalBoxScoreTeamsNew(boxscore["awayTeam"])
+
+    homeTeamData.update(_parseInternalBoxScorePlayersNew(
+        boxscore["boxscore"]["playerByGameStats"]["homeTeam"]
+    ))
+    awayTeamData.update(_parseInternalBoxScorePlayersNew(
+        boxscore["boxscore"]["playerByGameStats"]["awayTeam"]
+    ))
+
+    homeTeamData.update({
+        "gameId": boxscore["id"], 
+        "teamType": 1,
+        "winner": bool(homeTeamData["goals"] > awayTeamData["goals"])
+    })
+    awayTeamData.update({
+        "gameId": boxscore["id"], 
+        "teamType": 0,
+        "winner": not homeTeamData["winner"]
+    })
+
+    return homeTeamData, awayTeamData
+
+
 def parseRecentData(data, maxRecords=None, gameType=""):
     """Parse the results of games and return the wins, losses, winPercent, streak
     from that period indicated by maxRecords. The winPercent will always be greater
@@ -194,29 +344,42 @@ def parseArguments():
     """Parse the arguements for the program by reading in the static file that
     contains the basic statistics for all teams and all seasons.
     """
-    # Ask for the year/season for analysis
-    currentYear = datetime.now().year
+    validFiles = []
+    output = {}
     questions = [
-        inquirer.Text('startYear', message="Enter the year for the analysis to start.", default=2000),
-        inquirer.Text('endYear', message="Enter the year for the analysis to end.", default=currentYear-1),
+        inquirer.List('version', message="Select a version of data", choices=["new", "old"])
     ]
     answers = inquirer.prompt(questions)
-    startYear = int(answers["startYear"])
-    endYear = int(answers["endYear"])
+    output["version"] = answers["version"]
 
-    return startYear, endYear
+    if answers["version"] == "old":
+        # Ask for the year/season for analysis
+        currentYear = datetime.now().year
+        questions = [
+            inquirer.Text('startYear', message="Enter the year for the analysis to start.", default=2000),
+            inquirer.Text('endYear', message="Enter the year for the analysis to end.", default=currentYear-1),
+        ]
+        answers = inquirer.prompt(questions)
+        startYear = int(answers["startYear"])
+        endYear = int(answers["endYear"])
+        startYear, endYear = min([startYear, endYear]), max([startYear, endYear])
+        output.update({"startYear": startYear, "endYear": endYear})
 
+        for root, dirs, files in os.walk(directory):
+            sp = root.split("/")
+            try:
+                if startYear <= int(sp[len(sp)-1]) <= endYear:
+                    validFiles.extend([path_join(root, f) for f in files])
+            except:
+                pass
+    else:
+        # This is new so read the "NHLOpenSeason.json" file
+        validFiles.append(
+            path_join(*[dirname(abspath(__file__)), "NHLOpenSeason.json"])
+        )
 
+    return output, validFiles
 
-basePath = "neural_net"
-
-if exists(basePath):
-    rmtree(basePath)
-mkdir(basePath)
-
-
-startYear, endYear = parseArguments()
-startYear, endYear = min([startYear, endYear]), max([startYear, endYear])
 
 # data to be added to a dateframe and output to an excel file.
 totalData = []
@@ -224,62 +387,137 @@ totalData = []
 seasonParsedEvents = {}
 teamRecentWinPercents = defaultdict(list)
 
-validFiles = []
-for root, dirs, files in os.walk(directory):
+parsedOutput, validFiles = parseArguments()
+version = parsedOutput["version"]
+startYear = parsedOutput.get("startYear", None)
+endYear = parsedOutput.get("endYear", None)
 
-    sp = root.split("/")
-    try:
-        if startYear <= int(sp[len(sp)-1]) <= endYear:
-            validFiles.extend([path_join(root, f) for f in files])
-    except:
-        pass
+if version == "old":
+    for fname in validFiles:
 
-for fname in validFiles:
+        # find the directory and parse the events for this data 
+        splitPath = fname.split("/")
+        year = int(splitPath[len(splitPath)-2])
 
-    # find the directory and parse the events for this data 
-    splitPath = fname.split("/")
-    year = int(splitPath[len(splitPath)-2])
+        if year not in seasonParsedEvents:
+            parsedHomeTeamEvents, _ = parseSeasonEvents(year)
+            if None in (parsedHomeTeamEvents, ):
+                # TODO: something needs to happen if there is no data ... break?
+                print(f"failed to find data for {year}")
+                seasonParsedEvents[year] = {}
+            else:
+                seasonParsedEvents[year] = parsedHomeTeamEvents
 
-    if year not in seasonParsedEvents:
-        parsedHomeTeamEvents, _ = parseSeasonEvents(year)
-        if None in (parsedHomeTeamEvents, ):
-            # TODO: something needs to happen if there is no data ... break?
-            print(f"failed to find data for {year}")
-            seasonParsedEvents[year] = {}
-        else:
-            seasonParsedEvents[year] = parsedHomeTeamEvents
+        parsedHomeTeamEvents = {}
+        if year in seasonParsedEvents:
+            parsedHomeTeamEvents = seasonParsedEvents[year]
+        
 
-    parsedHomeTeamEvents = {}
-    if year in seasonParsedEvents:
-        parsedHomeTeamEvents = seasonParsedEvents[year]
-    
+        jsonData = None
+        with open(fname) as jsonFile:
+            jsonData = loads(jsonFile.read())
 
+        if jsonData:
+
+            localGameData = {}
+
+            gameInfo = jsonData["gameData"]
+            localGameData = {
+                "gameId": gameInfo["game"]["pk"]
+            }
+            boxScore = jsonData["liveData"]["boxscore"]
+            homeTeamData, awayTeamData = parseBoxScore(boxScore)
+            
+            homeTeamData.update({
+                "gameId": gameInfo["game"]["pk"], 
+                "teamType": 1,
+                "winner": bool(homeTeamData["goals"] > awayTeamData["goals"])
+            })
+            awayTeamData.update({
+                "gameId": gameInfo["game"]["pk"], 
+                "teamType": 0,
+                "winner": not homeTeamData["winner"]
+            })
+            
+            # The following calculates the wins/losses win percentage, any win/lose streak, total 
+            # win percentage (season), and the home or away win percentage (based on the type of game
+            # if the team is home or away). These are extra statistics that are not provided by the
+            # box score.
+            # NOTE: these calculations should be run before the current game information is added
+            wins, losses, winPercent, _ = parseRecentData(teamRecentWinPercents[homeTeamData['teamId']], 10)
+            _, _, totalWinPercent, streak = parseRecentData(teamRecentWinPercents[homeTeamData['teamId']])
+            _, _, winPercentHomeRecent, _ = parseRecentData(teamRecentWinPercents[homeTeamData['teamId']], 10, "H")
+            _, _, totalWinPercentHome, _ = parseRecentData(teamRecentWinPercents[homeTeamData['teamId']], None, "H")
+            homeTeamData.update({
+                "recentWinPercent": winPercent,
+                "totalWinPercent": totalWinPercent,
+                "gameTypeWinPercentRecent": winPercentHomeRecent,
+                "gameTypeWinPercent": totalWinPercentHome,
+                "streak": streak
+            })
+            wins, losses, winPercent, _ = parseRecentData(teamRecentWinPercents[awayTeamData['teamId']], 10)
+            _, _, totalWinPercent, streak = parseRecentData(teamRecentWinPercents[awayTeamData['teamId']])
+            _, _, winPercentAwayRecent, _ = parseRecentData(teamRecentWinPercents[awayTeamData['teamId']], 10, "A")
+            _, _, totalWinPercentAway, _ = parseRecentData(teamRecentWinPercents[awayTeamData['teamId']], None, "A")
+            awayTeamData.update({
+                "recentWinPercent": winPercent,
+                "totalWinPercent": totalWinPercent,
+                "gameTypeWinPercentRecent": winPercentAwayRecent,
+                "gameTypeWinPercent": totalWinPercentAway,
+                "streak": streak
+            })
+
+            # Now that the data above has been added to the list of features that _could_ determine
+            # the outcome of a game, add the actual outcome of the game and home vs away value
+            # to be used for the next calculations.
+            if homeTeamData["goals"] > awayTeamData["goals"]:
+                teamRecentWinPercents[homeTeamData["teamId"]].insert(0, ["W", "H"])
+                teamRecentWinPercents[awayTeamData["teamId"]].insert(0, ["L", "A"])
+            else:
+                teamRecentWinPercents[homeTeamData["teamId"]].insert(0, ["L", "H"])
+                teamRecentWinPercents[awayTeamData["teamId"]].insert(0, ["W", "A"])
+
+            # When performing the poisson distribution, an attack vs defense score was created.
+            # This _could_ be a useful statistic for predicting future game outcomes. The attack and
+            # defense score can be calculated for any team at any time during the season, so this
+            # means that there is less estimation involved.
+            if homeTeamData["teamId"] in parsedHomeTeamEvents:
+                for game in parsedHomeTeamEvents[homeTeamData["teamId"]]:
+                    if game.gameId == gameInfo["game"]["pk"]:
+                        homeTeamData.update({
+                            "attackStrength": game.homeAttackStrength, 
+                            "defenseStrength": game.homeDefenseStrength,
+                        })
+                        awayTeamData.update({
+                            "attackStrength": game.awayAttackStrength,
+                            "defenseStrength": game.awayDefenseStrength,
+                        })
+
+            totalData.extend([homeTeamData, awayTeamData])
+else:
+    # new version
     jsonData = None
-    with open(fname) as jsonFile:
+    with open(validFiles[0]) as jsonFile:
         jsonData = loads(jsonFile.read())
 
-    if jsonData:
+    year = None
+    for k, v in jsonData["boxScores"].items():
+        year = int(str(v["id"])[:4])
+        break
+    startYear = year
+    endYear = year
 
-        localGameData = {}
+    # start the new dictionary of data
+    seasonParsedEvents[year] = {}
+    parsedHomeTeamEvents = {}
 
-        gameInfo = jsonData["gameData"]
-        localGameData = {
-            "gameId": gameInfo["game"]["pk"]
-        }
-        boxScore = jsonData["liveData"]["boxscore"]
-        homeTeamData, awayTeamData = parseBoxScore(boxScore)
-        
-        homeTeamData.update({
-            "gameId": gameInfo["game"]["pk"], 
-            "teamType": 1,
-            "winner": bool(homeTeamData["goals"] > awayTeamData["goals"])
-        })
-        awayTeamData.update({
-            "gameId": gameInfo["game"]["pk"], 
-            "teamType": 0,
-            "winner": not homeTeamData["winner"]
-        })
-        
+    localGameData = {}
+    for _, boxscore in jsonData["boxScores"].items():
+        homeTeamData, awayTeamData = parseBoxScoreNew(boxscore)
+
+        # print(homeTeamData)
+        # print(awayTeamData)
+    
         # The following calculates the wins/losses win percentage, any win/lose streak, total 
         # win percentage (season), and the home or away win percentage (based on the type of game
         # if the team is home or away). These are extra statistics that are not provided by the
@@ -322,20 +560,21 @@ for fname in validFiles:
         # This _could_ be a useful statistic for predicting future game outcomes. The attack and
         # defense score can be calculated for any team at any time during the season, so this
         # means that there is less estimation involved.
-        if homeTeamData["teamId"] in parsedHomeTeamEvents:
-            for game in parsedHomeTeamEvents[homeTeamData["teamId"]]:
-                if game.gameId == gameInfo["game"]["pk"]:
-                    homeTeamData.update({
-                        "attackStrength": game.homeAttackStrength, 
-                        "defenseStrength": game.homeDefenseStrength,
-                    })
-                    awayTeamData.update({
-                        "attackStrength": game.awayAttackStrength,
-                        "defenseStrength": game.awayDefenseStrength,
-                    })
+        # if homeTeamData["teamId"] in parsedHomeTeamEvents:
+        #     for game in parsedHomeTeamEvents[homeTeamData["teamId"]]:
+        #         if game.gameId == gameInfo["game"]["pk"]:
+        #             homeTeamData.update({
+        #                 "attackStrength": game.homeAttackStrength, 
+        #                 "defenseStrength": game.homeDefenseStrength,
+        #             })
+        #             awayTeamData.update({
+        #                 "attackStrength": game.awayAttackStrength,
+        #                 "defenseStrength": game.awayDefenseStrength,
+        #             })
 
         totalData.extend([homeTeamData, awayTeamData])
-    
+
+
 
 # generate the dataframe and add to a spreadsheet
 
