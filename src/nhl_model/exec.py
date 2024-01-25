ca1d6fe06019f1e9979744880a030b53cf9082dc
Brent Barbachem <barbacbd@gmail.com>
2024-01-25 10:31:42 -0500

    DOCS: update doc strings/comments as some were copied over and included eroneous spaces.

--- src/nhl_model/exec.py
+++ src/nhl_model/exec.py
@@ -15,19 +15,19 @@ def main():
 
     mainSubParsers = parser.add_subparsers(dest='execType')
 
-    # ANN is the artificial neural network generation/loading. In the event of loading a model                                                                                                                                                
-    # the model will be used and MOST of the following parameters are not used. When the model                                                                                                                                                
-    # is not loaded (it will be created), and the following parameters are used for training a                                                                                                                                                
-    # model that will be used for predicting future game outcomes.                                                                                                                                                                            
+    # ANN is the artificial neural network generation/loading. In the event of loading a model
+    # the model will be used and MOST of the following parameters are not used. When the model
+    # is not loaded (it will be created), and the following parameters are used for training a
+    # model that will be used for predicting future game outcomes.
     annSubParser = mainSubParsers.add_parser('ann', help='Artificial Neural Network')
     annSubParser.add_argument(
         '--override', action='store_true', help='override all values in the config'
     )
 
-    # Generate a dataset used for the artificial neural network. The training data for the ANN should                                                                                                                                         
-    # contain a start and end year that are different than the current year/season. This will                                                                                                                                                 
-    # provide a good training dataset. When creating a dataset for prediction, set the end and start                                                                                                                                          
-    # year to the current season (ex. 2022-2023 season use 2022).                                                                                                                                                                             
+    # Generate a dataset used for the artificial neural network. The training data for the ANN should
+    # contain a start and end year that are different than the current year/season. This will
+    # provide a good training dataset. When creating a dataset for prediction, set the end and start
+    # year to the current season (ex. 2022-2023 season use 2022).
     generateSubParser = mainSubParsers.add_parser('generate', help='Generate Dataset')
     generateSubParser.add_argument(
         '-e', '--endYear', help='Season year where the dataset should end', default=datetime.now().year
@@ -39,9 +39,9 @@ def main():
         '-v', '--version', help='Version of the api where data will be pulled', default='new', choices=['old', 'new'],
     )
 
-    # Poisson distribution is used to predict the winner of a specific game based on the                                                                                                                                                      
-    # number of goals that each team will likely score during the game. This method uses                                                                                                                                                      
-    # the poisson distribution to predict the values and provide an output.                                                                                                                                                                   
+    # Poisson distribution is used to predict the winner of a specific game based on the
+    # number of goals that each team will likely score during the game. This method uses
+    # the poisson distribution to predict the values and provide an output.
     poissonSubParser = mainSubParsers.add_parser('poisson', help='Poisson Distribution')
     poissonSubParser.add_argument('-y', '--year', help='Year for the start of the season', default=datetime.now().year)
 
